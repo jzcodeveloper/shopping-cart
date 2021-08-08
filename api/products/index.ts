402699700d0576563e6cad0815d14f296686f53d
Javier Zabala <javier_bislip@hotmail.com>
2021-08-08 16:18:34 -0400

    Added comment to api functions

--- api/products/index.ts
+++ api/products/index.ts
@@ -2,6 +2,10 @@ import { AxiosError } from "axios";
 import { api } from "../../utils/api";
 import { Product } from "../../contexts/cart/types";
 
+/**
+ * @description Get products
+ * @route /products
+ */
 export const getProducts = async (): Promise<Product[]> => {
   try {
     const res = await api.get<Product[]>("/products");
