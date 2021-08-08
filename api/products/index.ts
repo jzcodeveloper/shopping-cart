c4869895a6cf90614444195099200387d3a87191
Javier Zabala <javier_bislip@hotmail.com>
2021-08-08 16:09:43 -0400

    Restored original getProducts endpoint

--- api/products/index.ts
+++ api/products/index.ts
@@ -4,7 +4,7 @@ import { Product } from "../../contexts/cart/types";
 
 export const getProducts = async (): Promise<Product[]> => {
   try {
-    const res = await api.get<Product[]>("/w21w21w2");
+    const res = await api.get<Product[]>("/");
     return res.data;
   } catch (error) {
     const axiosError = error as AxiosError;
