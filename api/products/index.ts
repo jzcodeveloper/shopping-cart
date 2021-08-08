671b2059fc666e72a856ef9902e3ff2d23bf6c53
Javier Zabala <javier_bislip@hotmail.com>
2021-08-08 16:00:52 -0400

    Return empty array on error

--- api/products/index.ts
+++ api/products/index.ts
@@ -2,13 +2,13 @@ import { AxiosError } from "axios";
 import { api } from "../../utils/api";
 import { Product } from "../../contexts/cart/types";
 
-export const getProducts = async (): Promise<Product[] | AxiosError> => {
+export const getProducts = async (): Promise<Product[]> => {
   try {
     const res = await api.get<Product[]>("/w21w21w2");
     return res.data;
   } catch (error) {
     const axiosError = error as AxiosError;
     console.error(axiosError.message);
-    return axiosError;
+    return [];
   }
 };
