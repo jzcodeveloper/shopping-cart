f8e6456486d16b2c0eee6bf83ef21688795f26d8
Javier Zabala <javier_bislip@hotmail.com>
2021-08-08 15:13:09 -0400

    Updated readme.md

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,6 @@
 import { AxiosRequestConfig } from "axios";
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "";
-export const REVALIDATE_TIME = 60 * 60 * 24;
 
 export const AXIOS_CONFIG: AxiosRequestConfig = {
   timeout: 10000,
