1b5a38cbe8b7d1a83328b8a178d5302409d2a9a4
Javier Zabala <javier_bislip@hotmail.com>
2021-08-08 15:56:19 -0400

    Add try catch block in api requests

--- api/products/index.ts
+++ api/products/index.ts
@@ -1,7 +1,14 @@
+import { AxiosError } from "axios";
 import { api } from "../../utils/api";
 import { Product } from "../../contexts/cart/types";
 
-export const getProducts = async (): Promise<Product[]> => {
-  const res = await api.get<Product[]>("/");
-  return res.data;
+export const getProducts = async (): Promise<Product[] | AxiosError> => {
+  try {
+    const res = await api.get<Product[]>("/w21w21w2");
+    return res.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    console.error(axiosError.message);
+    return axiosError;
+  }
 };
